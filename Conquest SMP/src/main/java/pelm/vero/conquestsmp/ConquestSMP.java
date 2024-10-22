58a5d4711427262284e7a78782ca4688d6489dbd
greglewis8 <133074154+greglewis8@users.noreply.github.com>
2024-10-21 18:34:22 -0700

    Plugin

---
+++ Conquest SMP/src/main/java/pelm/vero/conquestsmp/ConquestSMP.java
@@ -0,0 +1,17 @@
+package pelm.vero.conquestsmp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ConquestSMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("hi");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
